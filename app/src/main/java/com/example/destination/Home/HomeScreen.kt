9eefcf23ed143db0137cbbbc8219057866ee2fa2
Tushar Garg <garg31@uwindsor.ca>
2020-06-18 21:36:28 -0400

    Working on home screen

---
+++ app/src/main/java/com/example/destination/Home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.example.destination.Home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.destination.R
+
+class HomeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
--- app/src/main/java/com/example/destination/OnBoarding/Login/LoginActivity.kt
+++ app/src/main/java/com/example/destination/OnBoarding/Login/LoginActivity.kt
@@ -3,17 +3,19 @@ package com.example.destination.OnBoarding.Login
 import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.transition.Fade
 import android.transition.Transition
 import android.transition.TransitionManager
 import android.view.View
+import com.example.destination.Home.HomeScreen
 import com.example.destination.R
 import kotlinx.android.synthetic.main.activity_on_board.*
 
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +37,20 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun onClick() {
+        ivBrowseGuest.setOnClickListener(this)
+    }
 
+    override fun onClick(v: View?) {
+
+        when(v?.id){
+
+            R.id.ivBrowseGuest ->{
+                val intent = Intent(this,HomeScreen::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
 
     }
 
